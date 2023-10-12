77fbcf41c5357269dba300512ce19bd3dd29ff5a
BiancaMe <bianquis0410@gmail.com>
2023-10-12 17:50:57 -0300

    Fix linters errors

--- morse_code_decoder.rb
+++ morse_code_decoder.rb
@@ -1,29 +1,33 @@
-class MorseCodeDecoder
-    MORSE_TO_LETTER = {
-      '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
-      '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
-      '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
-      '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
-      '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
-      '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
-      '-.--' => 'Y', '--..' => 'Z'
-    }.freeze
-  
-    def decode_char(morse)
-      MORSE_TO_LETTER[morse]
-    end
-  
-    def decode_word(morse_word)
-      morse_word.split.map { |morse| decode_char(morse) }.join
-    end
+# frozen_string_literal: true
 
-    def decode(message)
-      words = message.split('   ')
-      decoded_words = words.map { |word| decode_word(word) }
-      decoded_words.join(' ')
-      end
-    end
-    decoder = MorseCodeDecoder.new
-    message = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
-    decoded_message = decoder.decode(message)
-    puts decoded_message
+# Class to Morse Code
+class MorseCodeDecoder
+  MORSE_TO_LETTER = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
+    '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
+    '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+    '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
+    '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+    '-.--' => 'Y', '--..' => 'Z'
+  }.freeze
+
+  def decode_char(morse)
+    MORSE_TO_LETTER[morse]
+  end
+
+  def decode_word(morse_word)
+    morse_word.split.map { |morse| decode_char(morse) }.join
+  end
+
+  def decode(message)
+    words = message.split('   ')
+    decoded_words = words.map { |word| decode_word(word) }
+    decoded_words.join(' ')
+  end
+end
+
+decoder = MorseCodeDecoder.new
+message = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+decoded_message = decoder.decode(message)
+puts decoded_message
